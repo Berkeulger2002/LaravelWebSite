b3291a6ecc0b9a859fe009ac68b9a30e9ba359b9
Berkeulger2002 <oberul.1ger@gmail.com>
2024-05-24 23:50:29 +0300

    Initial commit

---
+++ my_first_app/app/Http/Controllers/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+abstract class Controller
+{
+    //
+}
+namespace App\Http\Controllers;
+
+use OpenAdmin\Admin\Controllers\AdminController;
+
+class YourController extends AdminController
+{
+    // Your code here
+}
---
+++ my_first_app/resources/views/layouts/index.blade.php
@@ -0,0 +1,1 @@
+<h1>admin please</h1>
